b4791cfcae5109b4d454bfcec269faff9d0ba532
selfishgin <ahmedink02@gmail.com>
2025-02-21 16:04:00 +0400

    update

--- Common/obj/Release/net8.0/Common.AssemblyInfo.cs
+++ Common/obj/Release/net8.0/Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66c8d1cba24a5c89f72ee37c22bce0cc996193d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11336b6db17e18feb9fcec45c94bca63b1b8ea51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
